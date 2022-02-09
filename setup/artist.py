1f21f2b6fe647d0865797a8631db3c50a615967e
הדס <hadasharush214@gmail.com>
2022-02-09 09:58:44 +0200

    class for each artist

---
+++ setup/artist.py
@@ -0,0 +1,12 @@
+from setup import album
+
+
+class Artist:
+    def __init__(self, id: str, name: str, albums=[]):
+        self.id = id
+        self.name = name
+        self.albums = albums
+
+    def add_song(self, new_album: album.Album):
+        self.albums.append(new_album)
+
