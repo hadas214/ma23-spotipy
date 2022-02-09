473b5fb9d4379d0ff34beb2b10bec636bea193fe
הדס <hadasharush214@gmail.com>
2022-02-09 14:19:22 +0200

    fixed reader_file class

--- ETL/reader_file.py
+++ ETL/reader_file.py
@@ -7,16 +7,18 @@ import json
 class Reader(config_parser.Parser):
     def parse(self, folder):
         file_paths = [folder + path for path in os.listdir(folder)]
+        data = []
 
         for file_path in file_paths:
             with open(file_path, 'r') as file:
-                data = json.load(file)
-            print(data)
+                data.append(json.load(file))
+
+        return data
 
 
 # load property, search after value that called folder.songs in the property
-# folder = load_properties.Loader.load_properties('properties.properties', 'folder.songs'
+# folder = load_properties.Loader.load_properties('properties.properties', 'folder.songs')
 
 # read the folder that contains all the json files and load them
 # r = Reader()
-# r.parse(folder)
+# print(r.parse(folder))
